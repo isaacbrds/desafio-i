98657d2a8d9144f959b24c47a00703c8d1545c28
isaacbrds <isaac.brigido@gmail.com>
2022-03-29 09:10:19 -0300

    test: configurando testes do purchase items

--- spec/models/material_spec.rb
+++ spec/models/material_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe Material, type: :model do
   it { is_expected.to validate_presence_of(:title) }
   it { is_expected.to validate_uniqueness_of(:title) }
   it { is_expected.to validate_numericality_of(:quantity).is_greater_than_or_equal_to(0) }
+  
+  describe "associations" do
+    it { is_expected.to have_many(:purchase_items) }
+  end
 end
