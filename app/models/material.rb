f2b30e682ab97704d3327a7981c3a97cf3d73ccd
isaacbrds <isaac.brigido@gmail.com>
2022-03-29 19:51:21 -0300

    Feat: configurando model order item

--- app/models/material.rb
+++ app/models/material.rb
@@ -1,6 +1,10 @@
 class Material < ApplicationRecord
-  has_many :purchase_items
+  has_many :purchase_items, dependent: :restrict_with_exception
   has_many :purchases, through: :purchase_items
+  has_many :order_items, dependent: :restrict_with_exception
+  has_many :orders, through: :order_items
+  
+  paginates_per 10
   
   scope :for_title, ->(title) { where(["title ILIKE ?", "%#{title}%"]) }
   validates :title, presence: true, uniqueness: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(email: 'admin@mail.com', password: '123456', password_confirmation: '123456')
+User.create(email: 'user@mail.com', password: '123456', password_confirmation: '123456')
+User.create(email: 'almoxarife@mail.com', password: '123456', password_confirmation: '123456')
+
+puts "Usuário Criado"
+
+FactoryBot.create_list(:material, 20)
+
+puts "Materiais Criados"
+
